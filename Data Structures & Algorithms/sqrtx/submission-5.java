3993f362620819230865dab10d60958f490933b6
Yashi vijay vargiya <66182066+yashivijayvargiya@users.noreply.github.com>
2026-07-05 20:44:15 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left = 1;
+        int right = x/2;
+        int ans = 0;
+        if(x==1){return 1;}
+        while(left<=right){
+            int mid = left +(right-left)/2;
+            if(mid == x/mid){
+                return mid;
+            }
+            else if (mid < x/mid){
+                 ans = mid;
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return ans;
+        
+    }
+}
