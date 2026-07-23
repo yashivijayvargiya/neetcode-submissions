36b12917e4bf0ee10e0cd845ef15e624756d3224
Yashi vijay vargiya <66182066+yashivijayvargiya@users.noreply.github.com>
2026-07-24 01:43:12 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,37 @@
+class MinStack {
+    Stack<Integer> st;
+    Stack<Integer> minSt;
+
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+        
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        if(minSt.isEmpty() || val<=minSt.peek()){
+            minSt.push(val);
+        }
+        
+    }
+    
+    public void pop() {
+        if(st.peek().equals(minSt.peek())){
+            minSt.pop();
+        }
+        st.pop();
+        
+    }
+    
+    public int top() {
+        return st.peek();
+        
+    }
+    
+    public int getMin() {
+        return minSt.peek();
+
+        
+    }
+}
